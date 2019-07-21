4c5a70be0cdab9d9372cd88844ed77d2a92cce8d
m-kawahigashi <masa.15.30@icloud.com>
2019-07-21 20:24:23 +0900

    ルーティングを記述し、items_controllerにdetailアクションを追記した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+
+  def detail
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'tops#index'
+  get 'items/detail' => 'items#detail'
 end
