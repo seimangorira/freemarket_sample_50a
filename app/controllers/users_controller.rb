9b4d2ac838d277329a932e142f0beea8b4e0b0a5
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-08-01 20:25:24 +0900

    write-haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,7 @@ class UsersController < ApplicationController
   def profile
   end
 
+  def identification
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'tops#index'
   # resources :users
   get 'users/profile' => 'users#profile'
+  get 'users/identification' => 'users#identification'
 end
