7599d030935e7c8b97a9d61bad9d45996613a143
t-krt <cest1la6vieg@gmail.com>
2019-08-01 21:03:44 +0900

    Update deploy.rb
    作業中のブランチからデプロイできるようにコードを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "freemarket_sample_50a"
 set :repo_url, "git@github.com:seimangorira/freemarket_sample_50a.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
