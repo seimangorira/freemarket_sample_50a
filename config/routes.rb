26fbda8304b7d60fa541ed53827485a979c34cb3
t-krt <cest1la6vieg@gmail.com>
2019-08-03 17:13:50 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'tops#index'
-  get 'toppage/index' => 'toppage#index'
   resources :items, only: [:new]
   get 'users/logout' => 'users#logout'
   get 'users/profile' => 'users#profile'
