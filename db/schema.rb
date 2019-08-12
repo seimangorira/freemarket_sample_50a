ced8b3052cf568d34008b1889d0ae826f2bd0152
t-krt <cest1la6vieg@gmail.com>
2019-08-12 17:47:11 +0900

    ancestryカラムのNOT NULL制約を解除

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_025525) do
+ActiveRecord::Schema.define(version: 2019_08_12_041510) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry", null: false
+    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
