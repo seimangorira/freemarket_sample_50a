5a057a00a41c3d4244b921896c800e93f9d4091a
t-krt <cest1la6vieg@gmail.com>
2019-08-18 00:54:33 +0900

    Itemモデルへimagesの添付に関する処理を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
-  has_many :images
+  has_many_attached :images
+
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User"
 end
