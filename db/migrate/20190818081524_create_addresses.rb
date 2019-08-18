155c6d88389b4ed5a58e3170b0f4140e064b914b
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-08-18 17:43:04 +0900

    カラムを追加しました

--- db/migrate/20190818081524_create_addresses.rb
+++ db/migrate/20190818081524_create_addresses.rb
@@ -1,7 +1,16 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-
+      t.string  :first_name,         null: false
+      t.string  :first_name_kana,    null: false
+      t.string  :last_name,          null: false
+      t.string  :last_name_kana,     null: false
+      t.integer :postal_cord,        null: false
+      t.string  :state,              null: false
+      t.string  :municipalities,     null: false
+      t.string  :house_number,       null: false
+      t.string  :building_name
+      t.integer :phone_number,       unique: true
       t.timestamps
     end
   end
