b2d88306fdc98c49c727300d9ab30d0c1c9f597c
t-krt <cest1la6vieg@gmail.com>
2019-08-24 14:54:49 +0900

    商品出品時のバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,21 @@ class Item < ApplicationRecord
 
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
+
+  # 商品出品時、編集時のバリデーション
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal: 300, less_than_or_equal_to: 9999999 }
+  validates :introduction, presence: true, length: { maximum: 1000 }
+
+  with_options presence: true do
+    validates :state
+    validates :delivery_fee
+    validates :delivery_method
+    validates :city
+    validates :delivery_days
+    validates :category_id
+  end
+
+  # ActiveStorageのバリデーションは未実装
+  
 end
