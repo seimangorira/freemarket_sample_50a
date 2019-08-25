2bf4958d5fdf5e4ad736f549c4044f9e4c6e3d07
t-krt <cest1la6vieg@gmail.com>
2019-08-26 00:16:10 +0900

    販売手数料、販売利益を計算して表示
    入力された「販売価格」が¥300~¥9,999,999のときに、手数料と利益を表示するようにしました。

---
+++ app/assets/javascripts/calculation.js
@@ -0,0 +1,19 @@
+$(document).on("turbolinks:load", function(){
+  function feeCalculation(inputPrice){
+    var saleFee = inputPrice * 0.1;
+    var saleProfit = inputPrice - saleFee;
+
+    $('.sale-fee').text(`¥${saleFee}`);
+    $('.selling-item__profit--result').text(`¥${saleProfit}`)
+  }
+
+  $('#selling-item-price').on('keyup', function() {
+    var inputPrice = $('#selling-item-price').val();
+    if ( 300 <= inputPrice && inputPrice <= 9999999) {  //価格が¥300〜¥9,999,999のときにtextを書き換え
+      feeCalculation(inputPrice) 
+    } else { //それ以外のときは、textを'-'に書き換え
+      $('.sale-fee').text('-');
+      $('.selling-item__profit--result').text('-');
+    }
+  });
+});
