36c5f6bd11f66d9399c20054caa3daba40242bd7
t-krt <cest1la6vieg@gmail.com>
2019-08-27 22:18:52 +0900

    update: 「配送の方法」に関するclass名を修正

--- app/assets/javascripts/delivery.js
+++ app/assets/javascripts/delivery.js
@@ -1,5 +1,5 @@
 $(document).on("turbolinks:load", function(){
-  // 購入者が負担時の「配送の方法」フォームを作成
+  // 「着払い」時に利用する「配送の方法」フォームを作成
   function appendDeliveryMethodBuyer(){
      var deliveryMethod = ` <div id="add-delivery-method">
                               <label class="delivery-setting__burden">配送の方法
@@ -20,15 +20,15 @@ $(document).on("turbolinks:load", function(){
 
   // 「配送料の負担」選択後のイベント
   $('#delivery-fee').on('change', function(){
-    var deliveryBurden = $('#delivery-fee').val(); //選択された「配送の負担」のidのvalを取得
-    if (deliveryBurden == "postage_included") { //出品者負担が選択された場合の処理を記述
+    var deliveryBurden = $('#delivery-fee').val(); //「送料込み」、「着払い」のいずれであるかを取得
+    if (deliveryBurden == "postage_included") { //「送料込み」が選択された場合の処理を記述
       $('#add-delivery-method').remove();
-      $(".default-delivery-setting").css('display', 'block');
-    } else if (deliveryBurden == "cash_on_delivery") { //購入者負担が選択された場合の処理を記述
-      $(".default-delivery-setting").css('display', 'none');
+      $(".default-delivery-method").css('display', 'block');
+    } else if (deliveryBurden == "cash_on_delivery") { //「着払い」が選択された場合の処理を記述
+      $(".default-delivery-method").css('display', 'none');
       appendDeliveryMethodBuyer();
     } else { //「配送料の負担」が初期値になった時、「配送の方法」フォームを削除する
-      $(".default-delivery-setting").css('display', 'none');
+      $(".default-delivery-method").css('display', 'none');
       $('#add-delivery-method').remove(); 
     }
   });
