a2dfb0a951b2b5cc49146e05b3d3330a03cc7ca5
m-kawahigashi <masa.15.30@icloud.com>
2019-08-31 17:50:56 +0900

    コメントアウトの削除

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -9,11 +9,8 @@ class TopsController < ApplicationController
 
 private
   def get_items(id)
-    # Categoriesテーブルの親のidを取得。idは指定せず引数で指定する。indexアクションでid番号を持たせる。
     category = Category.find(id)
-    # indirect_idsメソッドを使って、親のidにヒットする子孫idを取得する処理(もしidが1(レディース)の場合、Categoriesテーブルの1(レディース)に紐づく全ての値(子供と孫のidとその値)を取得できる。
     categories = category.indirect_ids
-    # 変数itemsに、Itemsテーブルのcategory_idにヒットするidと値を取得する。
     items = Item.where(category_id: categories).order('id DESC').limit(4)
   end
 end
