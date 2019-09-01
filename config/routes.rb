56bdfefffb3f2cf81fc383e0e323b4d211258bf3
t-krt <cest1la6vieg@gmail.com>
2019-09-01 20:11:53 +0900

    add: 商品出品機能に必要なルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   get "users/card" => "users#card"
   get "users/addCard" => "users#addCard"
   resources :users, only: [:show]
-  resources :items, only: [:new, :create, :show] do
+  resources :items, only: [:new, :create, :show, :edit, :update] do
     collection do
       get 'get_children_categories', defaults: { format: 'json' }
       get 'get_grandchildren_categories', defaults: { format: 'json' }
