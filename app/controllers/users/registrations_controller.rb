b347a05936caee81b709d0bb6298113fad5cdeba
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-09-01 20:54:10 +0900

    デフォルトのアクションを使ってリダイレクトできるように記述

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,14 +5,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    # @user = User.new(user_params)
+    # if @user.save 
+    # else
+    #   render :new
+    # end
+    super
+  end
+
+  def complete
+  end
 
   # GET /resource/edit
   # def edit
@@ -51,12 +59,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    phonenumber_registration_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
+  #   binding.pry
+  #   phonenumber_registration_path
   # end
 end
