cb4ea19ee9c73c37337146aa0da3c56f8eff89a7
t-krt <cest1la6vieg@gmail.com>
2019-09-04 00:17:34 +0900

    fix: カラム名の修正に伴い、コードを一部修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :comments
   has_one :credit
   has_many :buyer_items, foreign_key: "buyer_id", class_name: "Item"
-  has_many :saling_items, -> { where("buyer_id is NULL")}, foreign_key: "saler_id", class_name: "Item"
+  has_many :saling_items, -> { where("buyer_id is NULL")}, foreign_key: "seller_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is NULL")}, foreign_key: "saler_id", class_name: "Item"
 end
