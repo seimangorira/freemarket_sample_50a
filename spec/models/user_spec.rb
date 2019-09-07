0eab27e30784dae571644fa21bc70853cc038e02
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-09-07 21:39:56 +0900

    テストエラーを解消しました

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,64 +8,44 @@ describe User do
     end
 
     it "nicknameが空では登録できない" do
-      user = build(:user, nickname: nil)
-      user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      expect(FactoryBot.build(:user, nickname: "")).to_not be_valid
     end
 
     it "first_nameが空では登録できない" do
-      user = build(:user, first_name: nil)
-      user.valid?
-      expect(user.errors[:first_name]).to include("can't be blank")
+      expect(FactoryBot.build(:user, first_name: "")).to_not be_valid
     end
 
     it "first_name_kanaが空では登録できない" do
-      user = build(:user, first_name_kana: nil)
-      user.valid?
-      expect(user.errors[:first_name_kana]).to include("can't be blank")
+      expect(FactoryBot.build(:user, first_name_kana: "")).to_not be_valid
     end
 
     it "last_nameが空では登録できない" do
-      user = build(:user, last_name: nil)
-      user.valid?
-      expect(user.errors[:last_name]).to include("can't be blank")
+      expect(FactoryBot.build(:user, last_name: "")).to_not be_valid
     end
 
     it "last_name_kanaが空では登録できない" do
-      user = build(:user, last_name_kana: nil)
-      user.valid?
-      expect(user.errors[:last_name_kana]).to include("can't be blank")
+      expect(FactoryBot.build(:user, last_name_kana: "")).to_not be_valid
     end
 
     it "emailが空では登録できない" do
-      user = build(:user, email: nil)
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      expect(FactoryBot.build(:user, email: "")).to_not be_valid
     end
 
     it "passwordが空では登録できない" do
-      user = build(:user, password: nil)
-      user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      expect(FactoryBot.build(:user, password: "")).to_not be_valid
     end
 
     it "passwordが存在してもpassword_confirmationが空では登録できない" do
-      user = build(:user, password_confirmation: "")
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+      expect(FactoryBot.build(:user, password_confirmation: "")).to_not be_valid
     end
 
     it "重複したemailが存在する場合登録できない" do
-      user = create(:user)
-      another_user = build(:user, email: user.email)
-      another_user.valid?
-      expect(another_user.errors[:email]).to include("has already been taken")
+      user1 = FactoryBot.create(:user, nickname: "菅田将暉", email: "sudamasaki@gmail.com")
+      expect(FactoryBot.build(:user, nickname: "ziro", email: user1.email)).to_not be_valid
     end
 
     it "passwordが6文字以下であれば登録できない" do
-      user = build(:user, password: "000000", password_confirmation: "000000")
-      user.valid?
-      expect(user.errors[:password][0]).to include("is too short")
+      expect(FactoryBot.build(:user, password: "0000", password_confirmation: "0000")).to_not be_valid
     end
   end
 end
