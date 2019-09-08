35defc5c6c59104004d4c009363fb76c037525cd
seimangorira <seimangorira@gmail.com>
2019-09-08 14:16:45 +0900

    トークンを発行する為のコントローラの作成

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,6 +1,14 @@
 class CreditsController < ApplicationController
   
   def index
+    Payjp.api_key = PAYJP_SECRET_KEY
+    customer = Payjp::Costomer.create(card: params[:payjp_token])
+    @payment = Payment.new(user_id: current_user.id, card_token: params[:payjp_token])
+    if @payment.save
+      redirect_to root_path, notice: "カードの作成が完了しました"
+    else
+      redirect_to new_user_cregit_path
+    end
   end
 
   def new
@@ -9,6 +17,11 @@ class CreditsController < ApplicationController
 
   def create
     # redirect_to complete_registration_path
+  end
+
+  private
+  def payment_params
+    params.require(:credit)
   end
 
 end
