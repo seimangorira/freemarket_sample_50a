e5d4c8a3c84531f71ba38f2f0d76b2ba5b99710f
m-kawahigashi <masa.15.30@icloud.com>
2019-09-09 16:36:54 +0900

    update exhibitions_controller.rb

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,13 +1,17 @@
 class ExhibitionsController < ApplicationController
+  before_action :set_item, only: [:show]
 
   def index
     @selling_items = Item.where(seller_id: current_user.id, status: 1).includes(:seller).order("created_at DESC")
   end
 
   def show
-    @item = Item.find(params[:id])
     @exhibitor = @item.seller
     @category = @item.category
+  end
+private
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
