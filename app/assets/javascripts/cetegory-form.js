6e95676b5feaf425fe94d98c52a44ea47564d1a6
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-09-11 21:22:00 +0900

    fix：select-range-name

--- app/assets/javascripts/cetegory-form.js
+++ app/assets/javascripts/cetegory-form.js
@@ -1,10 +1,10 @@
 $(document).on("turbolinks:load", function(){
 
-  $('.top-navbar-left__wrapper--list, .top-navbar-left__category-parent').mouseover(function() {
+  $('.top-navbar-left__wrapper--list__category, .top-navbar-left__category-parent').mouseover(function() {
     $('.top-navbar-left__category-parent').show();
   });
 
-  $('.top-navbar-left__wrapper--list, .top-navbar-left__category-parent').mouseleave(function(){
+  $('.top-navbar-left__wrapper--list__category, .top-navbar-left__category-parent').mouseleave(function(){
     $('.top-navbar-left__category-parent').hide();
   })
 
