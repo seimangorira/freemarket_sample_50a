ef9c1f20c197a00879aa446efbbdf002ff0a6c1b
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-09-11 18:25:02 +0900

    upgrade：mouseover-event

--- app/assets/javascripts/cetegory-form.js
+++ app/assets/javascripts/cetegory-form.js
@@ -1,31 +1,11 @@
 $(document).on("turbolinks:load", function(){
 
-  // 便利そうやけど、使い方がわからない
-  // function appendOption(category){
-  //   var option = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
-  //   return option;
-  // }
-
-  function appendParentCategory(){
-    var html = `<ul class="category-nav-parent-wrap" style="width: auto;">
-    <li class="category-nav-parent">
-      <h3>レディース</h3>
-    </li>
-    <li class="category-nav-parent">
-      <h3>メンズ</h3>
-    </li>
-  </ul>`
-    return html;
-  }
-
-  // $('a.category-search-form').mouseover(function() {
-  $('a.category-search-form').mouseover(function() {
-    // console.log("OK");
-    appendParentCategory;
+  $('.top-navbar-left__wrapper--list').mouseover(function() {
+    $('.top-navbar-left__category-parent').show();
   });
 
-  $('a.category-search-form').mouseleave(function(){
-    // console.log("わーい");
+  $('.top-navbar-left__wrapper--list').mouseleave(function(){
+    $('.top-navbar-left__category-parent').hide();
   })
 
 });
