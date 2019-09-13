249b29d284504eda4d43d61809f7fadbae6b7d6c
yoshibuchi <yoshibuchi.n@gmail.com>
2019-09-13 19:40:51 +0900

    コメントアウト削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,17 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_for(:google)
   end
 
-  # def google
-  #   @user = User.find_for_google(request.env['omniauth.auth'])
-
-  #   if @user.persisted?
-  #     flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-  #     sign_in_and_redirect @user, event: :authentication
-  #   else
-  #     session['devise.google_data'] = request.env['omniauth.auth']
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
+ 
 
   def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
@@ -37,29 +27,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to root_path and return
   end
 end
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+  
