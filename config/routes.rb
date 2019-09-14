d694ce8e75da5f085fb57dff9b7bcac59158f2f3
yoshibuchi <yoshibuchi.n@gmail.com>
2019-09-14 14:45:33 +0900

    微修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     post "/signup/credit/:id" => "credits#create", as: "credit_create_registration"
     get "/signup/complete" => "users/registrations#complete", as: "complete_registration"
   end
-  devise_for :users, controllers: { 
+  devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks",
     registrations: 'users/registrations',
     sessions: 'users/sessions'
@@ -26,9 +26,6 @@ Rails.application.routes.draw do
   get "users/addCard" => "users#addCard"
   get "users/logout" => "users#logout"
   resources :users, only: [:show]
-  
-
-  resources :items, only: [:new, :create, :show] do
   resources :exhibitions, only: [:index, :show]
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
     collection do
@@ -36,5 +33,4 @@ Rails.application.routes.draw do
       get 'get_grandchildren_categories', defaults: { format: 'json' }
     end
   end
-  resources :purchases, only: [:show]
 end
