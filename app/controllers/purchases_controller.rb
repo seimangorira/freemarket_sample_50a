aa9420d30aecf5a97c2b37f9e4b8ae1df8328c5f
seimangorira <seimangorira@gmail.com>
2019-09-15 17:56:07 +0900

    修正箇所の訂正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,19 +1,27 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:show, :update]
 
   # 商品確認ページの表示
   def show
-    @item = Item.find(params[:id])
     redirect_to root_path if @item.seller_id == current_user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     @user = User.find(current_user.id)
-    @credit = Credit.find(@user.credit.id)
-    Payjp.api_key = ENV['PAYJP_SECRET_ACCESS_KEY']
-    Payjp::Charge.create(currency: 'jpy', amount: @item.price, customer: @credit.customer_id)
-    @item.update(status: 4, buyer_id: current_user.id)
-    redirect_to root_path, notice: "購入が完了しました"
+    if @user.credit.present?
+      @credit = Credit.find(@user.credit.id)
+      Payjp.api_key = ENV['PAYJP_SECRET_ACCESS_KEY']
+      Payjp::Charge.create(currency: 'jpy', amount: @item.price, customer: @credit.customer_id)
+      @item.update(status: 4, buyer_id: current_user.id)
+      redirect_to root_path, notice: "購入が完了しました"
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
