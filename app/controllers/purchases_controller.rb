e690ed9ae8d23866514a6447588e42c6aa6b8fbd
seimangorira <seimangorira@gmail.com>
2019-09-15 14:23:13 +0900

    クレジットを使用して商品登録機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,4 +6,12 @@ class PurchasesController < ApplicationController
 
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @credit = Credit.find(1)
+    Payjp.api_key = ENV['PAYJP_SECRET_ACCESS_KEY']
+    Payjp::Charge.create(currency: 'jpy', amount: 1000, customer: @credit.customer_id)
+    redirect_to root_path
+  end
+
 end
