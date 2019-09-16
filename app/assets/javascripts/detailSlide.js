6b28f982a9820aa8a1d376cdd2ecf67b35ad8577
m-kawahigashi <masa.15.30@icloud.com>
2019-09-17 00:01:14 +0900

    hoverした時のイベント発火処理を追加。

--- app/assets/javascripts/detailSlide.js
+++ app/assets/javascripts/detailSlide.js
@@ -2,5 +2,7 @@ $(document).on("turbolinks:load", function(){
     $('.main__item-box__item-main__content__photo__image-box__mini-image').hover(function(){
       var num = $(this).data('image-id')
       $('.main__item-box__item-main__content__photo__header__items').animate({left: num * (-300)}, 500)
+      $('.main__item-box__item-main__content__photo__image-box__mini-image').removeClass("active"); //hoverした時に「active」をクラスから消す記述。
+      $(this).addClass('active') //hoverした時に「active」をクラスに追加する処理。
     });
 })
