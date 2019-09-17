36293061a9280ff2ea2d441041094a53f1b256cc
m-kawahigashi <masa.15.30@icloud.com>
2019-09-17 13:18:18 +0900

    コメントアウトの削除。

--- app/assets/javascripts/detailSlide.js
+++ app/assets/javascripts/detailSlide.js
@@ -1,14 +1,14 @@
 $(document).on("turbolinks:load", function(){
   $('.main__item-box__item-main__content__photo__image-box__mini-image').hover(function(){
-    var num = $(this).data('image-id');  //setTimeout関数内でthisが使えないので、ここで変数を指定。
-    var active = $(this);                //setTimeout関数内でthisが使えないので、ここで変数を指定。
-    timmer = setTimeout(function(){ //イベント発火を遅らせる
+    var num = $(this).data('image-id');
+    var active = $(this);
+    timmer = setTimeout(function(){
       $('.main__item-box__item-main__content__photo__header__items').animate({left: num * (-300)}, 500);
-      $('.main__item-box__item-main__content__photo__image-box__mini-image').removeClass('active'); //hoverした時に「active」をクラスから消す記述。
-      active.addClass('active'); //hoverした時に「active」をクラスに追加する処理。
-    },1000); //1秒後に処理を実行。
+      $('.main__item-box__item-main__content__photo__image-box__mini-image').removeClass('active'); 
+      active.addClass('active');
+    },1000);
   },
     function(){
-    clearTimeout(timmer); //1秒経つまでにhoverが外れたらイベント発火をキャンセル。
+    clearTimeout(timmer);
   });
 });
