582de2f555c9c9d3ebdf7c89d7553b11c3e1fa1d
seimangorira <seimangorira@gmail.com>
2019-09-18 21:51:10 +0900

    パスの変更
    phonenumber_registration_pathに飛ぶようにした

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -75,7 +75,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   def after_sign_up_path_for(resource)
-    root_path
+    phonenumber_registration_path
   end
 
   # The path used after sign up for inactive accounts.
