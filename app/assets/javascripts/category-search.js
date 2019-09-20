ff113615da22c529cfefbc24f6d705be04c39a7c
yyu8k <50798858+yyu8k@users.noreply.github.com>
2019-09-20 19:57:37 +0900

    add：append-children-category

--- app/assets/javascripts/category-search.js
+++ app/assets/javascripts/category-search.js
@@ -1,43 +1,84 @@
 $(document).on("turbolinks:load", function(){
 
+  // function buildChildHTML(child){
+  //   var html =`<a class="child_category" id="${child.id}" 
+  //               href="/category/${child.id}">${child.name}</a>`;
+  //   return html;
+  // }
+
   // カテゴリー検索
   $('.top-navbar-left__wrapper--list__category, .top-navbar-left__category__parent').mouseover(function() {
     $('.top-navbar-left__category__parent').show();
-    $('.parent-category-list').mouseover(function(){
-      var parentCategory = $(this).data("category-id"); //選択された親カテゴリのidを取得
-      console.log(parentCategory);
-      if (parentCategory != ""){ //親カテゴリが初期値（null）でないことを確認
-        $.ajax({
-          url: '/items/get_children_categories',
-          type: 'GET',
-          data: { parent_id: parentCategory },
-          dataType: 'json'
-        })
-        .done(function(children){
-          $('.parent-category-list').change(); //親が変更された時、子以下を削除する
-          $('.parent-category-list').change();
-          $('.top-navbar-left__category__children').css('display', 'block');
-          $('.top-navbar-left__category__children').css('display', 'none');
-          children.forEach(function(children){
-            ('.top-navbar-left__category__children');
-          });
-          ('.top-navbar-left__category__garndchildren');
-        })
-      }
-    })
   });
 
   $('.top-navbar-left__wrapper--list__category, .top-navbar-left__category__parent').mouseleave(function(){
     $('.top-navbar-left__category__parent').hide();
   })
 
-  // ブランド検索
-  $('.top-navbar-left__wrapper--list__brand, .top-navbar-left__brand').mouseover(function() {
-    $('.top-navbar-left__brand').show();
+  function buildChildHTML(child){
+    var html =`<li class="children-category-list" id="${child.id} href="/category/${child.id}">
+            <h2>${child.name}</h2></li>`;
+    return html;
+  }
+
+  $(".parent-category-list").on("mouseover", function() {
+    var parentCategory = $(this).data("category-id"); //選択された親カテゴリのidを取得
+    console.log(parentCategory);
+    // $(".now-selected-red").removeClass("now-selected-red")//赤色のcssのためです
+    // $('#' + parentCategory).addClass("now-selected-red");//赤色のcssのためです
+    $(".children-category-list").show();
+    $(".children-category-list").css('display', 'none');//一旦出ている子カテゴリ消します！
+    $(".grandchildren-category-list").css('display', 'none');//孫、てめえもだ！
+    $.ajax({
+      url: '/tops/get_children_categories',
+      type: 'GET',
+      data: { parent_id: parentCategory },
+      dataType: 'json'
+    })
+    .done(function(children){
+      children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
+        var html = buildChildHTML(child);//HTMLにして
+        $(".top-navbar-left__category__children").append(html);//リストに追加します
+        console.log(html);
+        // $(".children-category-list").show();
+      })
+    });
   });
 
-  $('.top-navbar-left__wrapper--list__brand, .top-navbar-left__brand').mouseleave(function(){
-    $('.top-navbar-left__brand').hide();
-  })
+  function buildGrandChildHTML(child){
+    var html =`<a class="grand_child_category" id="${child.id}"
+               href="/category/${child.id}">${child.name}</a>`;
+    return html;
+  }
+
+  $(document).on("mouseover", ".child_category", function () {//子カテゴリーのリストは動的に追加されたHTMLのため
+    var id = this.id
+    $(".now-selected-gray").removeClass("now-selected-gray");//灰色のcssのため
+    $('#' + id).addClass("now-selected-gray");//灰色のcssのため
+    $.ajax({
+      url: '/items/get_grandchildren_categories',
+      type: 'GET',
+      data: { child_id: childCategory },
+      dataType: 'json'
+    })
+    .done(function(children) {
+      children.forEach(function (child) {
+        var html = buildGrandChildHTML(child);
+        $(".grand_children_list").append(html);
+      })
+      $(document).on("mouseover", ".child_category", function () {
+        $(".grand_child_category").remove();
+      });
+    });
+  });  
+
+  // ブランド検索
+  // $('.top-navbar-left__wrapper--list__brand, .top-navbar-left__brand').mouseover(function() {
+  //   $('.top-navbar-left__brand').show();
+  // });
+
+  // $('.top-navbar-left__wrapper--list__brand, .top-navbar-left__brand').mouseleave(function(){
+  //   $('.top-navbar-left__brand').hide();
+  // })
 
 });
